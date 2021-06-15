5bedfc173469742ccf51be9e5c27391a8da754f0
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-06-15 17:33:16 -0300

    Index + Scrollbar + BurgerMenu

--- Javascript/Loader.js
+++ Javascript/Loader.js
@@ -1,3 +1,18 @@
 $( document ).ready(function(){
     $('#header').load('/CyberHydra/Page/header.html');
+
 });
+
+function burger_menu(x) {
+    x.classList.toggle("change");
+
+    if (document.getElementById("links-mobile").style.transform == "translateY(0%)") {
+        document.getElementById("links-mobile").style.transform = "translateY(-120%)";
+        document.body.style.overflowY = "scroll";
+        document.getElementById("arrow").style.display = "flex";
+    } else {
+        document.getElementById("links-mobile").style.transform = "translateY(-0%)";
+        document.body.style.overflowY = "hidden";
+        document.getElementById("arrow").style.display = "none";
+    }
+}
