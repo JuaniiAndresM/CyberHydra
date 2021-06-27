1a4fec84f4adf8e77d1bbc5cb8fc5b151b8d40db
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-06-26 23:14:41 -0300

    Info Acerca

--- Javascript/Loader.js
+++ Javascript/Loader.js
@@ -9,11 +9,9 @@ function burger_menu(x) {
 
     if (document.getElementById("links-mobile").style.transform == "translateY(0%)") {
         document.getElementById("links-mobile").style.transform = "translateY(-120%)";
-        document.body.style.overflowY = "scroll";
         document.getElementById("arrow").style.display = "flex";
     } else {
         document.getElementById("links-mobile").style.transform = "translateY(-0%)";
-        document.body.style.overflowY = "hidden";
         document.getElementById("arrow").style.display = "none";
     }
 }
